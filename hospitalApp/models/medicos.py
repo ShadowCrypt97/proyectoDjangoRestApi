9d76937e139aeee6b75051422853c8cd4a99e4ea
ShadowCrypt97 <sergio970228@gmail.com>
2022-09-15 21:55:29 -0500

    modificación nombre llave foranea de PERSONA_ID a persona_id en el modelo  medicos.py

--- hospitalApp/models/medicos.py
+++ hospitalApp/models/medicos.py
@@ -3,6 +3,6 @@ from .personas import Persona
 
 class Medico(models.Model):
     id_medico = models.AutoField(primary_key=True,unique=True,null=False,blank=False)
-    persona_id = models.ForeignKey(Persona,related_name='PERSONA_ID', on_delete=models.CASCADE)
+    persona_id = models.ForeignKey(Persona,related_name='persona_id', on_delete=models.CASCADE)
     especialidad = models.CharField('ESPECIALIDAD',max_length= 40,null=False,blank=False)
     fecha_registro = models.DateField('FECHA_REGISTRO',null=False,blank=False)
